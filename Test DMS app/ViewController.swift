052e9fbf0bd4143428c539b7de2ffa9dcf1f1054
DmitryMS <dmitryms@gmail.com>
2016-04-09 11:01:52 +0300

    Initial Commit

---
+++ Test DMS app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test DMS app
+//
+//  Created by DMITRY SADEEV on 09.04.16.
+//  Copyright © 2016 DMITRY SADEEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Test DMS appTests/Test_DMS_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_DMS_appTests.swift
+//  Test DMS appTests
+//
+//  Created by DMITRY SADEEV on 09.04.16.
+//  Copyright © 2016 DMITRY SADEEV. All rights reserved.
+//
+
+import XCTest
+@testable import Test_DMS_app
+
+class Test_DMS_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
